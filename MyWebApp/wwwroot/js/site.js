283ee14a594ddb4378dc40500a011521b5f034f6
JustWaveThings <justwavethings@proton.me>
2023-05-17 16:26:20 -0400

    Update to show JS displayed in app

--- MyWebApp/wwwroot/js/site.js
+++ MyWebApp/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const root = document.getElementsByClassName('container')[1];
+console.log(root);
+const hello = () => {
+	const h1 = document.createElement('h2');
+	h1.textContent = 'JavaScript has entered the chat.';
+	root.append(h1);
+};
+
+hello();
